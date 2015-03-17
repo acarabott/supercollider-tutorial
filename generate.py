1f07fd10eeaebe14f6a5916b18702f0831ce3d81
Arthur Carabott <arthurc@gmail.com>
2015-03-17 17:36:45 +0000

    nested structure

--- generate.py
+++ generate.py
@@ -9,32 +9,45 @@ master = Path("./master/")
 students = Path("./students/")
 me = Path("./me/")
 
-
 for p in [students, me]:
     print(80 * "-")
 
+    # delete old dirs
     if p.exists():
         shutil.rmtree(p.name)
         print("removed {}".format(p.name))
 
-    p.mkdir(0o755)
+    # create new dirs, no scd files
+    def ignore_scd(dir, files):
+        res = [f for f in files if Path(f).suffix == '.scd']
+        return res
 
+    shutil.copytree(master.name, p.name, ignore=ignore_scd)
     print(80 * "-")
 
-    for scd in master.glob('*.scd'):
-        new = p / scd.name
+# create new .scd files, without nostudent blocks
+for scd in master.glob('**/*.scd'):
+    for p in [students, me]:
+        new = p / '/'.join(scd.parts[1:])
         new.touch(0o755)
-        print("created {}".format(scd))
 
         with scd.open() as s, new.open('w') as n:
             in_nostudent = False
             for line in s:
                 if '{begin nostudent}' in line:
                     in_nostudent = True
+
+                    if p is me:
+                        n.write('//' + '=' * 10 + ' ')
+                        n.write(line)
                 elif '{end nostudent}' in line:
                     in_nostudent = False
+
+                    if p is me:
+                        n.write('//' + '-' * 10 + ' ')
+                        n.write(line)
                 else:
                     if p is me or (p is students and not in_nostudent):
                         n.write(line)
 
-            print("wrote {}".format(scd))
+        print("created {}".format(new))
